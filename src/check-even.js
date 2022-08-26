078f316152dd78ef8997209918311aebfadaf994
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-26 16:48:25 +0300

    chore(check-even.js): add import getRandomNum

--- src/check-even.js
+++ src/check-even.js
@@ -1,5 +1,6 @@
 import getAnswer from './getAnswer.js';
 import isEven from './isEven.js';
+import getRandomNum from "./getRandomNum.js";
 
 export default () => {
   const playerName = getAnswer('May I have your name? ');
@@ -7,7 +8,7 @@ export default () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 1; i <= 3; i += 1) {
-    const randomNum = Math.ceil(Math.random() * 100);
+    const randomNum = getRandomNum();
     console.log(`Question: ${randomNum}`);
     const correctAnswer = isEven(randomNum);
     const playerAnswer = getAnswer('Your answer: ');
