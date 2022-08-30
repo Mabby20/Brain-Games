e90921d7da6d7d25b955fde03b449ef2e7d89974
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-30 13:50:59 +0300

    feat(games): add new module game-progression.js

---
+++ src/games/game-progression.js
@@ -0,0 +1,23 @@
+import getRandomNum from '../getRandomNum.js';
+import gameCore from '../index.js';
+
+export default () => {
+  const task = 'What number is missing in the progression?';
+
+  const getArrWithAnswerQuestion = () => {
+    const arrWithProgression = [];
+    const startOfProgression = getRandomNum();
+    const progressionIndexChange = getRandomNum(5);
+
+    for (let i = startOfProgression; arrWithProgression.length < 10; i += progressionIndexChange) {
+      arrWithProgression.push(i);
+    }
+
+    const randIndexArr = Math.floor(Math.random() * arrWithProgression.length);
+    const correctAnswer = arrWithProgression[randIndexArr].toString();
+    arrWithProgression[randIndexArr] = '..';
+    const question = arrWithProgression.join(' ');
+    return [question, correctAnswer];
+  };
+  gameCore(task, getArrWithAnswerQuestion);
+};
