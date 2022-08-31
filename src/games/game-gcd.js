f5829f51829dcb11854d32a19faba75a1d2344cd
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 19:25:19 +0300

    refactor(game-gcd.js): split the function into parts

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,23 +1,27 @@
 import getRandomNum from '../helpers/getRandomNum.js';
 import gameCore from '../index.js';
 
-export default () => {
-  const task = 'Find the greatest common divisor of given numbers.';
+const task = 'Find the greatest common divisor of given numbers.';
 
-  const getArrWithAnswerQuestion = () => {
-    const firstNum = getRandomNum(10);
-    const secondNum = getRandomNum(10);
-    const question = `${firstNum} ${secondNum}`;
-    const smallestNum = firstNum > secondNum ? secondNum : firstNum;
-    let correctAnswer;
-
-    for (let i = 1; i <= smallestNum; i += 1) {
-      if (firstNum % i === 0 && secondNum % i === 0) {
-        correctAnswer = i;
-      }
+const getCorrectAnswer = (firstNum, secondNum) => {
+  const smallestNum = firstNum > secondNum ? secondNum : firstNum;
+  for (let i = smallestNum; i > 1; i -= 1) {
+    if (firstNum % i === 0 && secondNum % i === 0) {
+      return i;
     }
-    correctAnswer = correctAnswer.toString();
-    return [question, correctAnswer];
-  };
+  }
+  return 1;
+};
+
+const getArrWithAnswerQuestion = () => {
+  const firstNum = getRandomNum(100);
+  const secondNum = getRandomNum(100);
+  const question = `${firstNum} ${secondNum}`;
+  const correctAnswer = getCorrectAnswer(firstNum, secondNum);
+
+  return [question, correctAnswer.toString()];
+};
+
+export default () => {
   gameCore(task, getArrWithAnswerQuestion);
 };
