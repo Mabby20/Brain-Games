fcbeac1a283f4a8cbd8272f446143ef1fe430f52
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 19:48:24 +0300

    refactor(game-prime.js): split the function into parts

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,27 +1,29 @@
 import getRandomNum from '../helpers/getRandomNum.js';
 import gameCore from '../index.js';
 
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getCorrectAnswer = (num) => {
+  let isPrime = true;
+  if (num === 1 || num <= 0) {
+    isPrime = false;
+  }
+  for (let i = 2; i <= (num / 2); i += 1) {
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime;
+};
+
+const getArrWithAnswerQuestion = () => {
+  const randomNum = getRandomNum(100);
+  const question = randomNum;
+  const correctAnswer = getCorrectAnswer(randomNum) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
 export default () => {
-  const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-  const getArrWithAnswerQuestion = () => {
-    const randomNum = getRandomNum(100);
-    const question = randomNum;
-    let flag = true;
-
-    if (randomNum === 1 || randomNum <= 0) {
-      flag = false;
-    }
-
-    for (let i = 2; i <= (randomNum / 2); i += 1) {
-      if (randomNum % i === 0) {
-        flag = false;
-        break;
-      }
-    }
-
-    const correctAnswer = flag === true ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
   gameCore(task, getArrWithAnswerQuestion);
 };
