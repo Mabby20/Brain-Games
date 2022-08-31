4a351c020adea9fb8ca188d5a5a5ed1f4ea33a0b
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 18:40:13 +0300

    refactor(game-even.js): split the function into parts

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -2,14 +2,15 @@ import getRandomNum from '../helpers/getRandomNum.js';
 import isEven from '../helpers/isEven.js';
 import gameCore from '../index.js';
 
-export default () => {
-  const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const getArrWithAnswerQuestion = () => {
-    const randomNum = getRandomNum();
-    const question = randomNum;
-    const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
+const getArrWithAnswerQuestion = () => {
+  const randomNum = getRandomNum();
+  const question = randomNum;
+  const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => {
   gameCore(task, getArrWithAnswerQuestion);
 };
