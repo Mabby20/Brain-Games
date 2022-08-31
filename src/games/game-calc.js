81d35f7d819921c9d5eeb9b3aafb9ed966f31af9
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 18:11:39 +0300

    refactor(game-calc.js): split the function into parts

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -2,29 +2,31 @@ import getRandElemArr from '../helpers/getRandElemArr.js';
 import getRandomNum from '../helpers/getRandomNum.js';
 import gameCore from '../index.js';
 
-export default () => {
-  const task = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+const task = 'What is the result of the expression?';
 
-  const getArrWithAnswerQuestion = () => { //
-    const operators = ['+', '-', '*'];
-    const randomOperator = getRandElemArr(operators);
-    const firstOperand = getRandomNum();
-    const secondOperand = getRandomNum();
-    const question = `${firstOperand} ${randomOperator} ${secondOperand}`; // сгенерированный вопрос
-    let correctAnswer;
-    switch (randomOperator) {
-      case '+':
-        correctAnswer = (firstOperand + secondOperand).toString();
-        break;
-      case '-':
-        correctAnswer = (firstOperand - secondOperand).toString();
-        break;
-      case '*':
-        correctAnswer = (firstOperand * secondOperand).toString();
-        break;
-      default:
-    }
-    return [question, correctAnswer];
-  };
+const getCorrectAnswer = (firstNum, secondNum, operator) => {
+  switch (operator) {
+    case '+':
+      return firstNum + secondNum;
+    case '-':
+      return firstNum - secondNum;
+    case '*':
+      return firstNum * secondNum;
+    default:
+  }
+  return firstNum + secondNum;
+};
+
+const getArrWithAnswerQuestion = () => { //
+  const randomOperator = getRandElemArr(operators);
+  const firstOperand = getRandomNum();
+  const secondOperand = getRandomNum();
+  const question = `${firstOperand} ${randomOperator} ${secondOperand}`; // сгенерированный вопрос
+  const correctAnswer = getCorrectAnswer(firstOperand, secondOperand, randomOperator);
+  return [question, correctAnswer.toString()];
+};
+
+export default () => {
   gameCore(task, getArrWithAnswerQuestion);
 };
