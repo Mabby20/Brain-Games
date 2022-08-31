b8c160ebb2b32486d9b1bc8b205b0c91748361c7
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 19:57:36 +0300

    refactor(game-progression.js): split the function into parts

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,23 +1,24 @@
 import getRandomNum from '../helpers/getRandomNum.js';
 import gameCore from '../index.js';
 
+const task = 'What number is missing in the progression?';
+
+const getArrWithAnswerQuestion = () => {
+  const arrWithProgression = [];
+  const startOfProgression = getRandomNum();
+  const progressionIndexChange = getRandomNum(5);
+
+  for (let i = startOfProgression; arrWithProgression.length < 10; i += progressionIndexChange) {
+    arrWithProgression.push(i);
+  }
+
+  const randIndexArr = getRandomNum(arrWithProgression.length, 'floor');
+  const correctAnswer = arrWithProgression[randIndexArr];
+  arrWithProgression[randIndexArr] = '..';
+  const question = arrWithProgression.join(' ');
+  return [question, correctAnswer.toString()];
+};
+
 export default () => {
-  const task = 'What number is missing in the progression?';
-
-  const getArrWithAnswerQuestion = () => {
-    const arrWithProgression = [];
-    const startOfProgression = getRandomNum();
-    const progressionIndexChange = getRandomNum(5);
-
-    for (let i = startOfProgression; arrWithProgression.length < 10; i += progressionIndexChange) {
-      arrWithProgression.push(i);
-    }
-
-    const randIndexArr = Math.floor(Math.random() * arrWithProgression.length);
-    const correctAnswer = arrWithProgression[randIndexArr].toString();
-    arrWithProgression[randIndexArr] = '..';
-    const question = arrWithProgression.join(' ');
-    return [question, correctAnswer];
-  };
   gameCore(task, getArrWithAnswerQuestion);
 };
