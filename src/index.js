88be759c992d288316f5607f7535c9c907f31bb4
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-08-31 13:29:08 +0300

    refactor(index.js): change name constant

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ export default (task, getArrWithAnswerQuestion) => {
   console.log(task);
   const numberOfAttempts = 3;
   for (let i = 1; i <= numberOfAttempts; i += 1) {
-    const pairOfQuestionAnswer = getArrWithAnswerQuestion();
+    const getQuestionWithAnswer = getArrWithAnswerQuestion();
     // На каждой итерации формируем новый вопрос и ответ.
     // Функция формирования ответа и вопроса у каждой игры своя.
-    console.log(`Question: ${pairOfQuestionAnswer[0]}`);
+    console.log(`Question: ${getQuestionWithAnswer[0]}`);
     const playerAnswer = getAnswer('Your answer: ');
-    if (playerAnswer === pairOfQuestionAnswer[1]) {
+    if (playerAnswer === getQuestionWithAnswer[1]) {
       console.log('Correct!');
     } else {
-      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${pairOfQuestionAnswer[1]}".`);
+      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${getQuestionWithAnswer[1]}".`);
       console.log(`Let's try again, ${playerName}!`);
       return;
     }
