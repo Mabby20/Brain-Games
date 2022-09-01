c5b3cb984de60642b8496248dd88db23ab3d8ae0
Pavel Kolotiy <Kolotiy199420@mail.ru>
2022-09-01 15:16:26 +0300

    refactor(getRandomNum.js): use function reduction

--- src/helpers/getRandomNum.js
+++ src/helpers/getRandomNum.js
@@ -1,9 +1,1 @@
-export default (index = 10, method = 'ceil') => {
-  if (method === 'ceil') {
-    return Math.ceil(Math.random() * index);
-  }
-  if (method === 'floor') {
-    return Math.floor(Math.random() * index);
-  }
-  return Math.ceil(Math.random() * index);
-};
+export default (index = 10, method = 'ceil') => (method === 'ceil' ? Math.ceil(Math.random() * index) : Math.floor(Math.random() * index));
